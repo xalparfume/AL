400772347238d8a98feae08fe81e2430ca6ba92a
xalparfume <ddghazyal@gmail.com>
2026-01-16 13:57:47 +0700

    Update fish.lua

--- fish.lua
+++ fish.lua
@@ -1,40 +1,18 @@
---[[ 
-   FISH IT NOTIFIER - FINAL STABLE VERSION
-   Metode: Name Filter (Paling Akurat)
-   Fitur: Footer "XAL Webhook" + Clean Text Discord
-]]
-
 local Webhook_URL = "https://discord.com/api/webhooks/1454735553638563961/C0KfomZhdu3KjmaqPx4CTi6NHbhIjcLaX_HpeSKqs66HUc179MQ9Ha_weV_v8zl1MjYK"
 
--- Daftar Ikan (Tambahkan nama baru di sini jika ada update)
-local SecretFishList = {
-    "Orca",
-    "Crystal Crab",
-    "Monster Shark", -- Ikan di screenshot kamu
-    "Eerie Shark",
-    "Great Whale",
-    "Robot Kraken",
-    "King Crab",
-    "Queen Crab",
-    "Kraken",
-    "Grand Maja",
-    "Bone Whale",
-    "Worm Fish",
-    "Ghost Shark",
-    "Megalodon",
-    "Skeleton Narwhal",
-    "Ruby",
+local TargetColors = {
+    "rgb(24, 255, 152)",
 }
 
--- =======================================================
--- LOGIKA SCRIPT
--- =======================================================
+local TargetNames = {
+    "Ruby",
+    "GEMSTONE Ruby",
+}
 
 local HttpService = game:GetService("HttpService")
 local StarterGui = game:GetService("StarterGui")
 local httpRequest = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
 
--- Fungsi Hapus Kode Warna (Biar Rapi di Discord)
 local function StripTags(str)
     return string.gsub(str, "<[^>]+>", "")
 end
@@ -44,11 +22,11 @@ local function SendWebhook(cleanMsg)
 
     local embedData = {
         ["username"] = "XAL APP",
-        ["avatar_url"] = "https://i.imgur.com/4M7IwwP.png",
+        ["avatar_url"] = "https://i.imgur.com/GWx0mX9.jpeg",
         ["embeds"] = {{
             ["title"] = "🐟 XAL Fish It Alert.!",
             ["description"] = "New Fish Caught !\n\n**" .. cleanMsg .. "**",
-            ["color"] = 3447003, -- Warna Biru (Supaya beda dikit)
+            ["color"] = 3447003,
             ["footer"] = { 
                 ["text"] = "XAL Webhook" 
             },
@@ -65,35 +43,41 @@ local function SendWebhook(cleanMsg)
 end
 
 local function CheckAndSend(msg)
-    -- Bersihkan dulu tag warnanya agar pencarian nama lebih mudah
-    local cleanMsg = StripTags(msg) 
-    local lowerMsg = string.lower(cleanMsg)
+    if not (string.find(msg, "obtained an") or string.find(msg, "chance!")) then return end
 
-    if string.find(lowerMsg, "obtained an") or string.find(lowerMsg, "chance!") then
+    local shouldSend = false
+    
+    for _, colorCode in pairs(TargetColors) do
+        if string.find(msg, colorCode) then
+            shouldSend = true
+            break
+        end
+    end
+
+    if not shouldSend then
+        local cleanMsg = StripTags(msg)
+        local lowerMsg = string.lower(cleanMsg)
         
-        local isSecret = false
-        for _, fishName in pairs(SecretFishList) do
-            -- Kita cari nama ikan di teks yang SUDAH BERSIH
+        for _, fishName in pairs(TargetNames) do
             if string.find(lowerMsg, string.lower(fishName)) then
-                isSecret = true
+                shouldSend = true
                 break
             end
         end
+    end
 
-        if isSecret then
-            SendWebhook(cleanMsg) -- Kirim teks bersih ke Discord
-            
-            -- Notifikasi di layar HP
-            StarterGui:SetCore("SendNotification", {
-                Title = "Webhook Terkirim!",
-                Text = cleanMsg,
-                Duration = 5
-            })
-        end
+    if shouldSend then
+        local finalMsg = StripTags(msg)
+        SendWebhook(finalMsg)
+        
+        StarterGui:SetCore("SendNotification", {
+            Title = "XAL Catch Detected!",
+            Text = finalMsg,
+            Duration = 5
+        })
     end
 end
 
--- LISTENER (Mendengar Chat Server)
 local TextChatService = game:GetService("TextChatService")
 TextChatService.OnIncomingMessage = function(message)
     if message.TextSource == nil then CheckAndSend(message.Text) end
@@ -109,9 +93,8 @@ if ChatEvents then
     end
 end
 
--- Tanda Script Jalan
 StarterGui:SetCore("SendNotification", {
     Title = "XAL Webhook Aktif",
-    Text = "Mode: Name Filter ",
+    Text = "Mode: Hybrid (Color + Name)",
     Duration = 5
 })
