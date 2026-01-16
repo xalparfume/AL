599aa1102dace1d588f49694d98cfa6295b8134e
xalparfume <ddghazyal@gmail.com>
2026-01-16 14:33:17 +0700

    Update cnf.lua

--- cnf.lua
+++ cnf.lua
@@ -1,14 +1,7 @@
---[[ 
-   FILENAME: cnf.lua
-   DESKRIPSI: Config User
-]]
-
--- Variabel diganti jadi 'CNF' agar sesuai nama file
 getgenv().CNF = {
     -- 1. Webhook
-    Webhook_URL = "MASUKKAN_URL_WEBHOOK_DISCORD_KAMU_DI_SINI",
+    Webhook_URL = "https://discord.com/api/webhooks/1454735553638563961/C0KfomZhdu3KjmaqPx4CTi6NHbhIjcLaX_HpeSKqs66HUc179MQ9Ha_weV_v8zl1MjYK",
 
-    -- 2. Daftar Secret
     SecretList = {
         "Orca",
         "Crystal Crab",
@@ -27,7 +20,6 @@ getgenv().CNF = {
         "Skeleton Narwhal",
     },
 
-    -- 3. Daftar Stone/Item
     StoneList = {
         "Ruby",
         "Sandy Ruby",
@@ -35,9 +27,4 @@ getgenv().CNF = {
     }
 }
 
--- =============================================================
--- LOADER: PANGGIL FILE LOGIC (xal.lua)
--- Masukkan Link Raw xal.lua di bawah ini
--- =============================================================
-
-loadstring(game:HttpGet("PASTE_LINK_RAW_GITHUB_XAL_LUA_DISINI"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xalparfume/AL/refs/heads/main/xal.lua"))()
