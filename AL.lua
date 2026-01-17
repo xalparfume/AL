c750eba858dc302c9781e6c2c91db3f79ad9b3c9
xalparfume <ddghazyal@gmail.com>
2026-01-17 10:57:56 +0700

    Create AL.lua

---
+++ AL.lua
@@ -0,0 +1,35 @@
+getgenv().CNF = {
+    Webhook_Fish = "https://discordapp.com/api/webhooks/1461678791364579405/Fw2q3tD0g-Vjrg1chacp69RlKWyz11f2GWG55TTHdEjcfx4F5ZE0aoxCjhsRwEJE60gJ",
+    Webhook_Leave = "https://discordapp.com/api/webhooks/1461679018482077793/teXw8CyPt33ffoIoQB1DzhMHeXOGDcylFFtHbjBFK3nWLrwDnIm2nSLH1T8_rdpy-qBP",
+    Webhook_List = "https://discordapp.com/api/webhooks/1461756167285117190/HeO0k35cPyKN-AA4d9TpLRZ7CDslwVngovlmzSsCsuBIjM_BDWyV3M2a8PB-77vAQ3Ri",
+
+    DiscordID_List = {
+        ["salepmieayam"] = "1379072004690284564",
+        ["xalparfume"]     = "1379072004690284564",
+        ["iPengedar"]  = "1379072004690284564",
+        ["zapelass123"]  = "444484989661478932",
+        ["oliviagilbass"]  = "444484989661478932",
+        ["bHpwfROqaruC"]  = "444484989661478932",
+        ["FleursMeron"]  = "661155146474192896",
+        ["yaaxjmn"]  = "661155146474192896",
+    },
+
+    SecretList = {
+        "Crystal Crab", "Orca", "Zombie Shark", "Zombie Megalodon", "Dead Zombie Shark",
+        "Blob Shark", "Ghost Shark", "Skeleton Narwhal", "Ghost Worm Fish", "Worm Fish",
+        "Megalodon", "1x1x1x1 Comet Shark", "Bloodmoon Whale", "Lochness Monster",
+        "Monster Shark", "Eerie Shark", "Great Whale", "Frostborn Shark", "Armored Shark",
+        "Scare", "Queen Crab", "King Crab", "Cryoshade Glider", "Panther Eel",
+        "Giant Squid", "Depthseeker Ray", "Robot Kraken", "Mosasaur Shark", "King Jelly",
+        "Bone Whale", "Elshark Gran Maja", "Ancient Whale", "Gladiator Shark",
+        "Ancient Lochness Monster", "Talon Serpent", "Hacker Shark", "ElRetro Gran Maja",
+        "Strawberry Choc Megalodon", "Krampus Shark", "Emerald Winter Whale",
+        "Winter Frost Shark", "Icebreaker Whale", "Leviathan", "Pirate Megalodon",
+    },
+
+    StoneList = {
+        "Ruby",
+    }
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xalparfume/AL/refs/heads/main/xal.lua"))()
