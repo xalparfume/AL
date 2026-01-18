4dcd23540f79995f15ee177b58357bfa5944aa3c
xalparfume <ddghazyal@gmail.com>
2026-01-18 07:52:22 +0700

    Create xload.lua

---
+++ xload.lua
@@ -0,0 +1,47 @@
+getgenv().CNF = {
+    Webhook_Fish = "https://discordapp.com/api/webhooks/1462043794018140313/ekiX6dPCBWy1ixKpaxnAa9OSZQaUh1XWaV2U6rjyxAvOcW5dvFIbB_sHT3Ot1sOUL11_",
+    Webhook_Leave = "https://discordapp.com/api/webhooks/1462043967393763484/3R9tJAsJNsLTT0gw63sqj36Kevi1TUf6uku3AEG9_l0VuJ8iNqdfb9sJ8MoHzTdc6MmV",
+    Webhook_List = "https://discordapp.com/api/webhooks/1462044102253084863/B-YCdKHJh1ud_4JaHG3lX6IMkoPpdx4hWPhHEjouE-ZRioLyoMygurUTDrS1erfvWC_3",
+
+    DiscordID_List = {
+        ["salepmieayam"] = "1379072004690284564",
+        ["xalparfume"]     = "1379072004690284564",
+        ["sayamaulodon3"]  = "990328404052156426",
+        ["zapelass123"]  = "444484989661478932",
+        ["Khalidramzy1"]  = "1079745021810585701",
+        ["RaisSky"]  = "1204756382965043253",
+        ["HatedBeloved"]  = "510861175613816863",
+        ["HatedDeGrace"]  = "510861175613816863",
+        ["babiluuu1"]  = "459941231448031242",
+        ["Fennn_00"]  = "1397449245883105360",
+        ["Ryuuk_a"]  = "1397449245883105360",
+        ["oliviagilbass"]  = "444484989661478932",
+        ["wingchong18"]  = "897846156778962944",
+        ["nessa12772"]  = "897846156778962944",
+        ["cansya3"]  = "388336911758000138",
+        ["reivenzie"]  = "459941231448031242",
+        ["aizzul24"]  = "1204756382965043253",
+        ["kaelnih3"]  = "1359315214587138079",
+        ["ademsarisachet"]  = "344836360022130689",
+        ["Taloncuih"]  = "990328404052156426",
+    },
+
+    SecretList = {
+        "Crystal Crab", "Orca", "Zombie Shark", "Zombie Megalodon", "Dead Zombie Shark",
+        "Blob Shark", "Ghost Shark", "Skeleton Narwhal", "Ghost Worm Fish", "Worm Fish",
+        "Megalodon", "1x1x1x1 Comet Shark", "Bloodmoon Whale", "Lochness Monster",
+        "Monster Shark", "Eerie Shark", "Great Whale", "Frostborn Shark", "Armored Shark",
+        "Scare", "Queen Crab", "King Crab", "Cryoshade Glider", "Panther Eel",
+        "Giant Squid", "Depthseeker Ray", "Robot Kraken", "Mosasaur Shark", "King Jelly",
+        "Bone Whale", "Elshark Gran Maja", "Ancient Whale", "Gladiator Shark",
+        "Ancient Lochness Monster", "Talon Serpent", "Hacker Shark", "ElRetro Gran Maja",
+        "Strawberry Choc Megalodon", "Krampus Shark", "Emerald Winter Whale",
+        "Winter Frost Shark", "Icebreaker Whale", "Leviathan", "Pirate Megalodon",
+    },
+
+    StoneList = {
+        "Ruby",
+    }
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xalparfume/AL/refs/heads/main/xal.lua"))()
